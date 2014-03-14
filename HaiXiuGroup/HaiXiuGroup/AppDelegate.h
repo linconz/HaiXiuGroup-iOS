40f6612885c7d047fea733608279b9dcb6734029
zhangyl <zhangyl@m15.cn>
2014-03-14 15:28:38 +0800

    initialize project

---
+++ HaiXiuGroup/HaiXiuGroup/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HaiXiuGroup
+//
+//  Created by zhang on 14-3-14.
+//  Copyright (c) 2014年 zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HaiXiuGroup/HaiXiuGroup/UIClasses/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  HaiXiuGroup
+//
+//  Created by zhang on 14-3-14.
+//  Copyright (c) 2014年 zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
